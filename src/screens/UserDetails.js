9829e457a134a658ce35015163c6979ef35e3141
Rameez Qazi <rameez.qazi23@gmail.com>
2021-02-04 23:22:43 +0500

    UI changes...

--- src/screens/UserDetails.js
+++ src/screens/UserDetails.js
@@ -1,90 +1,98 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView } from 'react-native';
 import { Button, Input } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 //Become a Donor
 function UserDetails() {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', textAlign: 'center' }}>
+        <ScrollView alwaysBounceVertical={true}>
+            <View style={{flex: 1, width: 500, height: 500}}>
+                <Text>Hello World</Text>
 
-            <View style={{ borderStyle: 'solid', borderRadius: 15, borderWidth: 1, width: 300, justifyContent: 'center', alignSelf: 'center' }}>
-                <View style={{ marginTop: 20 }}>
-                    <Input
-                        placeholder='Full Name'
-                        leftIcon={
-                            <Icon
-                                name='user'
-                                type='evilicon'
-                                color='#517fa4'
-                            />
-                        }
-                    />
-
-                </View>
-                <View style={{ width: 200, justifyContent: 'center' }}>
-                    <Input
-                        placeholder='Email'
-                        autoCompleteType='email'
-                        leftIcon={
-                            <Icon
-                                name='user'
-                                type='evilicon'
-                                color='#517fa4'
-                            />
-                        }
-                    />
-
-                </View>
-                <View style={{ width: 200, justifyContent: 'center' }}>
-                    <Input
-                        placeholder='Phone'
-                        keyboardType='number-pad'
+            </View>
 
 
-                        leftIcon={
-                            <Icon
-                                name='user'
-                                type='evilicon'
-                                color='#517fa4'
-                            />
-                        }
-                    />
+            <View style={{ flex: 1, justifyContent: 'center', textAlign: 'center' }}>
 
-                </View>
-                <View style={{ flexDirection: 'row', flexWrap: 'nowrap', width: 150, justifyContent: 'center', alignSelf: 'center', marginBottom: 30 }}>
-                    <Input
-                        placeholder='Age'
-                        keyboardType='number-pad'
-                        leftIcon={
-                            <Icon
-                                name='user'
-                                type='evilicon'
-                                color='#517fa4'
-                            />
-                        }
-                    />
-                    <Input
-                        placeholder='Weight'
-                        keyboardType='number-pad'
-                        leftIcon={
-                            <Icon
-                                name='user'
-                                type='evilicon'
-                                color='#517fa4'
-                            />
-                        }
-                    />
+                <View style={{ borderStyle: 'solid', borderRadius: 15, borderWidth: 1, width: 300, justifyContent: 'center', alignSelf: 'center' }}>
+                    <View style={{ marginTop: 20 }}>
+                        <Input
+                            placeholder='Full Name'
+                            leftIcon={
+                                <Icon
+                                    name='user'
+                                    type='evilicon'
+                                    color='#517fa4'
+                                />
+                            }
+                        />
+
+                    </View>
+                    <View style={{ width: 200, justifyContent: 'center' }}>
+                        <Input
+                            placeholder='Email'
+                            autoCompleteType='email'
+                            leftIcon={
+                                <Icon
+                                    name='user'
+                                    type='evilicon'
+                                    color='#517fa4'
+                                />
+                            }
+                        />
+
+                    </View>
+                    <View style={{ width: 200, justifyContent: 'center' }}>
+                        <Input
+                            placeholder='Phone'
+                            keyboardType='number-pad'
+
+
+                            leftIcon={
+                                <Icon
+                                    name='user'
+                                    type='evilicon'
+                                    color='#517fa4'
+                                />
+                            }
+                        />
+
+                    </View>
+                    <View style={{ flexDirection: 'row', flexWrap: 'nowrap', width: 150, justifyContent: 'center', alignSelf: 'center', marginBottom: 30 }}>
+                        <Input
+                            placeholder='Age'
+                            keyboardType='number-pad'
+                            leftIcon={
+                                <Icon
+                                    name='user'
+                                    type='evilicon'
+                                    color='#517fa4'
+                                />
+                            }
+                        />
+                        <Input
+                            placeholder='Weight'
+                            keyboardType='number-pad'
+                            leftIcon={
+                                <Icon
+                                    name='user'
+                                    type='evilicon'
+                                    color='#517fa4'
+                                />
+                            }
+                        />
+
+                    </View>
+
+
 
                 </View>
 
 
 
             </View>
-
-
-
-        </View>
+        </ScrollView>
 
     )
 }
